9d90c0fdad1a732f1e773e17934c25fde3374d81
unknown <ojitdebnathfreelancer.com>
2022-11-24 20:13:33 +0600

    produt category id api created

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express');
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Resell phones server is running')
 });
+// default api for check  
 
-app.listen(port, ()=>{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r7d25w3.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const resell = async () =>{
+    try{
+        const categoryNameData = client.db('resellphones').collection('categoryName');
+        const productsData = client.db('resellphones').collection('products');
+
+        app.get('/categories', async(req, res)=>{
+            const categoryName = await categoryNameData.find({}).toArray();
+            res.send(categoryName);
+        });
+        // get all category name 
+
+        app.get('/category/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query ={category_id:id};
+            const category = await productsData.find(query).toArray();
+            res.send(category);
+        });
+    }
+    finally{
+
+    }
+};
+resell().catch(error => console.error(error));
+
+
+app.listen(port, () => {
     console.log('server running', port)
 });
